c821a40e0ed39dc03dd9d64c28cff11274eea6e3
WANANDA DILLA ZAHRA <110005462+WanandaDillaZahra@users.noreply.github.com>
2022-09-27 12:43:04 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
 <?php
-    echo "<h1>Hello World</h1>";
+
+require_once('function/helper.php');
+require_once('function/koneksi.php');
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="<?= BASE_URL . 'assets/style01.css' ?>">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,500;0,700;0,900;1,400;1,500;1,700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="topbar">
+        <h3 class="text-topbar">@_wandza</h3>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#5000ca" fill-opacity="1" d="M0,192L21.8,192C43.6,192,87,192,131,208C174.5,224,218,256,262,245.3C305.5,235,349,181,393,149.3C436.4,117,480,107,524,128C567.3,149,611,203,655,229.3C698.2,256,742,256,785,229.3C829.1,203,873,149,916,133.3C960,117,1004,139,1047,170.7C1090.9,203,1135,245,1178,261.3C1221.8,277,1265,267,1309,256C1352.7,245,1396,235,1418,229.3L1440,224L1440,0L1418.2,0C1396.4,0,1353,0,1309,0C1265.5,0,1222,0,1178,0C1134.5,0,1091,0,1047,0C1003.6,0,960,0,916,0C872.7,0,829,0,785,0C741.8,0,698,0,655,0C610.9,0,567,0,524,0C480,0,436,0,393,0C349.1,0,305,0,262,0C218.2,0,175,0,131,0C87.3,0,44,0,22,0L0,0Z"></path></svg>
+    <div class="content">
+        <div class="card-login">
+            <div class="card-main">
+                <div class="card-header">Form Login</div>
+                <div class="card-body">
+                    <form class="form-login" method="POST" action="<?= BASE_URL . 'process/process_login.php' ?>">
+                        <label class="form-label">Username</label>
+                        <input type="username" name="username" class="form-input">
+                        <label class="form-label">Password</label>
+                        <input type="password" name="password" class="form-input">
+                        <button type="submit" class="btn btn-login">Login</button>
+                    </form>
+                    <p style="text-align: center;">Belum punya akun?<span><a href="<?= BASE_URL . "register.php" ?>" class=""> Daftar disini</a></span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
